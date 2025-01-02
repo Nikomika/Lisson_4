5bda01aa723a63bd064027ab0a395d1048b2b3cb
Nikomika <o.a.voytova@gmail.com>
2025-01-02 19:12:39 +0300

    make new function

--- src/Animals.java
+++ src/Animals.java
@@ -2,14 +2,85 @@ public class Animals {
     String name;
     int runDistant;
     int swimDistant;
-    public Animals (String name) {
-    this.name = name;
+    static int listAnimals;
+
+    public Animals(String name) {
+        this.name = name;
+        listAnimals++;
     }
-    public void runHappy () {
-        System.out.println(name + " пробежал " + runDistant);
+
+    public void setRunDistant(int runDistant) {
+        this.runDistant = runDistant;
     }
-    public void swimHappy () {
-        System.out.println (name + " проплыл "+ swimDistant);
+
+    public void setSwimDistant(int swimDistant) {
+        this.swimDistant = swimDistant;
+    }
+
+    public void runHappy() {
+        System.out.println(name + " пробежал " + runDistant + " метров");
+    }
+
+    public void swimHappy() {
+        System.out.println(name + " проплыл " + swimDistant + " метров");
+    }
+
+    public static int getListAnimals() {
+        return listAnimals;
     }
 }
+
+class Cat extends Animals {
+    public Cat(String name) {
+        super(name);
+        listAnimals++;
+    }
+    static int listAnimals;
+
+    @Override
+    public void swimHappy() {
+        System.out.println(name + " не умеет плавать");
+    }
+
+    public void runHappy() {
+        if (runDistant > 0 & runDistant <= 200) {
+            System.out.println(name + " пробежал " + runDistant + " метров");
+        } else {
+            System.out.println(name + " не может пробежать " + runDistant + " метров");
+        }
+    }
+
+    public static int getListAnimals() {
+        return listAnimals;
+    }
+}
+
+class Dog extends Animals {
+    public Dog(String name) {
+        super(name);
+        listAnimals++;
+    }
+    static int listAnimals;
+
+    @Override
+    public void swimHappy() {
+        if (swimDistant <= 10 & runDistant > 0) {
+            System.out.println(name + " проплыл " + swimDistant + " метров");
+        } else {
+            System.out.println(name + " не может проплыть " + swimDistant + " метров");
+        }
+    }
+    public void runHappy() {
+        if (runDistant <= 500 & runDistant > 0) {
+            System.out.println(name + " пробежал " + runDistant + " метров");
+        } else {
+            System.out.println(name + " не может пробежать " + runDistant + " метров");
+        }
+    }
+
+    public static int getListAnimals() {
+        return listAnimals;
+    }
+}
+
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        Animals Cat1 = new Animals("Киллик");
-        Cat1.runHappy();
-        Cat1.swimHappy();
+        Animals bird = new Animals("Киллик");
+        Animals bird2 = new Animals("Киллик2");
+        Animals bird3 = new Animals("Киллик3");
+        bird.setRunDistant(100);
+        bird.setSwimDistant(5);
+        bird.runHappy();
+        bird.swimHappy();
+        Cat firstCat = new Cat("TimSoner");
+        firstCat.swimHappy();
+        firstCat.setRunDistant(50);
+        firstCat.runHappy();
+        Dog barsFirst = new Dog("Барс");
+        barsFirst.setRunDistant(340);
+        barsFirst.setSwimDistant(3);
+        barsFirst.runHappy();
+        barsFirst.swimHappy();
+        System.out.println(Animals.getListAnimals());
+        System.out.println(Cat.getListAnimals());
+        System.out.println(Dog.getListAnimals());
+
     }
 }
